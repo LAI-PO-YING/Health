c8903bb013da4520b411cfadd610552fd7b0bcbb
Ivan Lai <229705jack2gmail.com>
2021-03-17 15:31:51 +0800

    Initial Commit

---
+++ Health/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Health
+//
+//  Created by Lai Po Ying on 2021/3/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
